ba11dd8b694294c18ec1fbf67a1bcb7c733f7a16
Dirk Kutzer <kutzer.dirk@gmail.com>
2017-10-26 21:59:12 +0200

    upgrade to oc 10

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ ADD setupApacheSSLKey.sh /usr/sbin/setup-apache-ssl-key
 ENV SUBJECT /C=US/ST=CA/L=CITY/O=ORGANIZATION/OU=UNIT/CN=localhost
 ENV DO_SSL_LETS_ENCRYPT_FETCH false
 ENV EMAIL fail
-RUN setup-apache-ssl-key
 ENV DO_SSL_SELF_GENERATION true
+RUN setup-apache-ssl-key
 
 # for https (apache)
 EXPOSE 443
@@ -22,14 +22,20 @@ ENV ENABLE_DAV true
 ENV ENABLE_CRON true
 CMD start-servers; sleep infinity
 
+ADD install-pacaur-pre.sh /usr/sbin/install-pacaur-pre
 ADD install-pacaur.sh /usr/sbin/install-pacaur
+RUN install-pacaur-pre
 RUN install-pacaur
 
 # set environmnt variable defaults
-ENV TARGET_SUBDIR owncloud
-ENV OC_VERSION '9.1.2-1'
 
 # do the install things
+ADD install-owncloud-pre.sh /usr/sbin/install-owncloud-pre
+RUN install-owncloud-pre
+
+ENV OC_VERSION '10.0.2'
+ENV TARGET_SUBDIR owncloud
+ADD PKGBUILD_OWNCLOUD /home/owncloud/PKGBUILD_OWNCLOUD
 ADD install-owncloud.sh /usr/sbin/install-owncloud
 RUN install-owncloud
 
--- install-owncloud.sh
+++ install-owncloud.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 set -x -e -u -o pipefail
 
-# remove info.php (prevents server info leak)
-rm /srv/http/info.php
-
-# to mount SMB shares: 
-pacman -S --noconfirm --noprogress --needed smbclient 
-
-# for video file previews
-pacman -S --noconfirm --noprogress --needed ffmpeg
-
-# for document previews
-pacman -S --noconfirm --noprogress --needed libreoffice-fresh
-
-# for image previews
-pacman -S --noconfirm --noprogress --needed imagemagick ghostscript openexr openexr openexr libxml2 librsvg libpng libwebp
-
-# not 100% sure what needs this:
-pacman -S --noconfirm --noprogress --needed gamin
-
 # owncloud itself
-gpg --recv-key 2D5D5E97F6978A26
+#gpg --recv-key 2D5D5E97F6978A26
 su owncloud -c 'gpg --recv-key 2D5D5E97F6978A26'
-su owncloud -c 'pacaur -m --noprogressbar --noedit --noconfirm owncloud-archive'
-pacman -U --noconfirm --needed /home/owncloud/.cache/pacaur/owncloud-archive/owncloud-archive-${OC_VERSION}-any.pkg.tar.xz
+
+
+# Install "owncloud" from AUR using local build file
+su owncloud -c 'BUILDDIR=/home/owncloud && makepkg PKGBUILD_OWNCLOUD'
+su owncloud -c 'pacman -U owncloud-archive*.tar.xz --noconfirm --needed --noedit -m --noprogressbar'
+
+#su owncloud -c 'pacaur -m --noprogressbar --noedit --noconfirm owncloud-archive'
+#pacman -U --noconfirm --needed /home/owncloud/.cache/pacaur/owncloud-archive/owncloud-archive-${OC_VERSION}-any.pkg.tar.xz
 
 # install some apps
 pacman -S --noconfirm --noprogress --needed owncloud-app-bookmarks owncloud-app-calendar owncloud-app-contacts owncloud-app-documents
